4f489fb0c681ea7cacdac6d56b0d8f45d3fd2c24
haleyepperlyfox <haleykepperly@gmail.com>
2023-01-05 07:49:50 -0800

    Adding solution to part 2 of day 1

--- code/day_1_code.R
+++ code/day_1_code.R
@@ -4,7 +4,7 @@
 library(tidyverse)
 
 # read in data
-data <- read.table("/Users/haleyfox/Documents/advent_of_code/data/day_1_data.txt", sep = '\t', blank.lines.skip = FALSE, header = FALSE)
+data <- read.table("https://raw.githubusercontent.com/haleyepperlyfox/advent_of_code_2022/main/data/day_1_data.txt", sep = '\t', blank.lines.skip = FALSE, header = FALSE)
 
 # add column that sums row i with i+1
 data_1 <- data %>% 
@@ -24,5 +24,10 @@ data_3 <- data_2 %>%
   mutate(real_sum = (new - lag(new))) %>% 
   arrange(desc(real_sum))
 
-# look at first row, column 2 to get answer
+# look at first row, column 2 to find elf with the highest calorie snacks
 head(data_3[1,2])
+
+# sum the top 3 rows to get the sum of calories carried by the top 3 elves
+data_3 %>%
+  slice_head(n = 3) %>%
+  summarise(sum = sum(real_sum))
